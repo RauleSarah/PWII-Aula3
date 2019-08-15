f033ad0db22e9a2bc96901b7d56d103529c2d3d1
RauleSarah <raulglitzk26@gmail.com>
2019-08-14 21:40:20 -0300

    Revisão - TS

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,10 +1,16 @@
-var nota = 9;
-var nota2 = 7;
-var nota3 = 5;
-var media = (nota + nota2 + nota3) / 3;
-if (media >= 5) {
-    console.log("Você foi aprovado!, e a sua média foi " + media);
+var n1 = 9;
+var n2 = 7;
+var n3 = 5;
+/*let media = (nota + nota2 + nota3)/3
+
+//if(media >= 5){
+    console.log("Você foi aprovado!, e a sua média foi " + media)
 }
-else {
-    console.log("Você foi reprovado, sua média foi, " + media);
-}
+else{
+    console.log("Você foi reprovado, sua média foi, " + media)
+}*/
+var fMedia = function (n1, n2, n3) {
+    return (n1 + n2 + n3) / 3;
+};
+//console.log("A média é: " + fMedia(n1,n2,n3))
+console.log("A media e: " + fMedia(n1, n2, n3));
--- revisao.ts
+++ revisao.ts
@@ -1,11 +1,16 @@
-let nota = 9
-let nota2 = 7
-let nota3 = 5
-let media = (nota + nota2 + nota3)/3
+let n1 = 9
+let n2 = 7
+let n3 = 5
+/*let media = (nota + nota2 + nota3)/3
 
-if(media >= 5){
-        console.log("Você foi aprovado!, e a sua média foi " + media)
+//if(media >= 5){
+    console.log("Você foi aprovado!, e a sua média foi " + media)
 }
 else{
     console.log("Você foi reprovado, sua média foi, " + media)
+}*/
+let fMedia = function(n1:number, n2:number, n3:number) : number {
+    return (n1 + n2 + n3) / 3
 }
+//console.log("A média é: " + fMedia(n1,n2,n3))
+console.log(`A media e: ${fMedia(n1,n2,n3)}`)
