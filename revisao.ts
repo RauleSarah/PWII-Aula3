5f96c6cf3f0ef352e879143532920a75340b1bcc
RauleSarah <raulglitzk26@gmail.com>
2019-08-14 22:00:13 -0300

    Revisão - TS

--- revisao.ts
+++ revisao.ts
@@ -2,15 +2,18 @@ let n1 = 9
 let n2 = 7
 let n3 = 5
 /*let media = (nota + nota2 + nota3)/3
-
+------------------- MODO SEM FUNCTION IF ELSE -------------------
 //if(media >= 5){
     console.log("Você foi aprovado!, e a sua média foi " + media)
 }
 else{
     console.log("Você foi reprovado, sua média foi, " + media)
 }*/
-let fMedia = function(n1:number, n2:number, n3:number) : number {
+//----------------- MODO BÁSICO COM FUNCTION ---------------------
+/*let fMedia = function(n1:number, n2:number, n3:number) : number {
     return (n1 + n2 + n3) / 3
 }
-//console.log("A média é: " + fMedia(n1,n2,n3))
+console.log("A média é: " + fMedia(n1,n2,n3))*/
+//----------------- MODO SIMPLIFICADO COM FUNCTION ---------------------------
+let fmedia = (n1:number,n2:number,n3:number) => (n1 + n2 + n3) / 3
 console.log(`A media e: ${fMedia(n1,n2,n3)}`)
